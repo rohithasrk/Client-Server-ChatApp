042eff4a4a8ae76f1689701f87cd52117b18c83b
Rohith Asrk <rohith.asrk@gmail.com>
2016-06-14 02:40:56 +0530

    Added Client Script and modified server script

--- Client.java
+++ Client.java
@@ -2,24 +2,24 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-class Server{
+public class Client{
 	public static void main(String args[]) throws Exception{
 		Scanner in=new Scanner(System.in);
-		ServerSocket ServSoc=new ServerSocket(8000);			//Server waiting for a connection-request to be made
-		Socket s=ServSoc.accept();					//To establish a connection
+		Socket s=new Socket("localhost",8000);
 		DataInputStream Din=new DataInputStream(s.getInputStream());
 		DataOutputStream Dout=new DataOutputStream(s.getOutputStream());
+		
 		String str="";
 		String str2="";
+		System.out.println("Connection established. You can now chat with the server. Type 'end' when you wish to end the chat.");
 		while(!str.equals("end")){
-			str=Din.readUTF();
-			System.out.println("Client says: "+str);
-			str2=in.nextLine();
-			Dout.writeUTF(str2);
-			Dout.flush();	
+			str=in.nextLine();
+			Dout.writeUTF(str);
+			Dout.flush();
+			str2=Din.readUTF();
+			System.out.println("Server says: "+str2);
 		}
-		Din.close();
-		s.close();
-		ServSoc.close();
+	Dout.close();
+	s.close();
 	}
 }
